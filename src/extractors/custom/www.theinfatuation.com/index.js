c92a0668a091f7f2f54446e035c7b1e1cd8ee0fc
Gus <gus.cuddy@gmail.com>
2022-03-02 20:52:53 -0500

    Changed clean header formula

--- src/extractors/custom/www.theinfatuation.com/index.js
+++ src/extractors/custom/www.theinfatuation.com/index.js
@@ -52,14 +52,6 @@ export const WwwTheinfatuationComExtractor = {
     // Is there anything in the content you selected that needs transformed
     // before it's consumable content? E.g., unusual lazy loaded images
     transforms: {
-      // remove img
-      /**
-       *
-       * @param {HTMLElement} $node
-       */
-      img: $node => {
-        $node.remove();
-      },
       // turn noscript into figure
       noscript: ($node, $) => {
         const $children = $.browser ? $($node.text()) : $node.children();
@@ -74,11 +66,11 @@ export const WwwTheinfatuationComExtractor = {
         return null;
       },
     },
-
+    defaultCleaner: false,
     // Is there anything that is in the result that shouldn't be?
     // The clean selectors will remove anything that matches from
     // the result
-    clean: [],
+    clean: ["img[src^='position:absolute']", "p[class*='photoCreds']"],
   },
   extend: {
     score: {
--- src/utils/dom/clean-headers.js
+++ src/utils/dom/clean-headers.js
@@ -10,10 +10,10 @@ export default function cleanHeaders($article, $, title = '') {
     // document. This probably means that it was part of the title, a
     // subtitle or something else extraneous like a datestamp or byline,
     // all of which should be handled by other metadata handling.
-    if ($($header, $article).prevAll('p').length === 0) {
-      return $header.remove();
-    }
-
+    // TODO: commenting this out because it was causing problems. look for better solution
+    // if ($($header, $article).prevAll('p').length === 0) {
+    //   return $header.remove();
+    // }
     // Remove any headers that match the title exactly.
     if (normalizeSpaces($(header).text()) === title) {
       return $header.remove();
